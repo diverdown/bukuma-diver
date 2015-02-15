74f53965d4dc83aa20f8efda0a000d616285a8a5
en30 <en30.git@gmail.com>
2015-02-15 23:56:34 +0900

    adds pending state to pages which have many comments

--- source/components/page.vue
+++ source/components/page.vue
@@ -9,6 +9,15 @@
   font-size: 10px;
   margin: 0.6em 0;
   box-shadow: -1px 1px 3px 1px #eee inset;
+  .pending {
+    text-align: center;
+    font-size: 1.2em;
+    a {
+      display: block;
+      text-decoration: underline;
+      font-size: 1.4em;
+    }
+  }
   .loading {
     padding: 2em;
     img { width: 6em; height: 6em; }
@@ -29,8 +38,13 @@
         | {{title | truncate 100}}
     a.bookmark-count(href="{{url | hatebuEntry}}" target="_blank") {{bookmark_count}}users
   .comments
+    .pending(v-if="pending")
+      | ブックマーク数が多いため読み込みを手動にしました。
+      a(v-on="click: loadComments") コメントを読み込む
+      | コメントの読み込みには時間がかかることがあります。
+
     loading-circle(v-if="loading")
-    table(v-if="!loading")
+    table(v-if="loaded")
       tr(v-repeat="comments" v-component="comment")
   a.small(v-on="click: openModal(domain)" v-if="showPopularLink") このサイトの人気ページを見る
 </template>
@@ -38,15 +52,24 @@
 <script lang="coffee">
 BukumaDiver = require '../bukuma_diver'
 _ = require 'lodash'
+PENDING_THRESHOULD = 2000
+PENDING = 0
+LOADING = 1
+LOADED = 2
 module.exports =
   data: ->
     comments: []
-    loading: true
+    state: LOADING
+  computed:
+    pending: -> @state == PENDING
+    loading: -> @state == LOADING
+    loaded: -> @state == LOADED
   methods:
     openModal: (domain)->
       @$dispatch 'openModal', domain
     loadComments: ->
-      BukumaDiver.comments(@url, (err, @comments)=> @loading = false)
+      @state = LOADING
+      BukumaDiver.comments(@url, (err, @comments)=> @state = LOADED)
     wasSeen: ->
       @unbindScroll() unless @$el
       {top, bottom} = @$el.getBoundingClientRect()
@@ -56,6 +79,7 @@ module.exports =
     unbindScroll: ->
       window.removeEventListener 'scroll', @_loadCommentIfInsideWindow
   attached: ->
+    return @state = PENDING if @bookmark_count > PENDING_THRESHOULD
     # to wait rendering and get appropriate position
     @$root.constructor.nextTick =>
       return @loadComments() if @wasSeen()
