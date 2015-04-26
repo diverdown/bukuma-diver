0baaaa037dde5baf731b59f88f25e8a134a73f12
en30 <en30.git@gmail.com>
2015-04-26 14:55:27 +0900

    Sinatra::NotFoundは無視

--- api/app.rb
+++ api/app.rb
@@ -18,6 +18,7 @@ if production?
   require 'raven'
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
+    config.excluded_exceptions = %w(Sinatra::NotFound)
   end
   use Raven::Rack
 end
